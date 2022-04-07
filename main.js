ed97798b9aacb5eb1bff43c849ef260ee8fda204
Jennyfer C Santos <33470911+jennyarch@users.noreply.github.com>
2022-04-06 21:03:01 -0300

    My First Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function tocaSom (seletorAudio) { /* Nomes que tenham sentido para o parametro */
+  const elemento = document.querySelector(seletorAudio)
+  
+  if(elemento != null && elemento.localName === 'audio'){
+      elemento.play();
+  }
+  else {
+      console.log('Elemento não encontrado ou seletor invalido')
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function (){
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento){
+
+    if(evento.code ==='Space' || evento.code === 'Enter'){
+      tecla.classList.add('ativa');
+    }
+  }
+  tecla.onkeyup = () => {
+      tecla.classList.remove('ativa');
+  }
+
+}
